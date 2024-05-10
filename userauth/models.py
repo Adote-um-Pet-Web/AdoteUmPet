fcd459c18a61b86949de45ed7985fb290707ea3b
wendellast <wendelalves01a@hotmail.com>
2024-05-10 17:11:05 -0300

    login email --ok

--- userauth/models.py
+++ userauth/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-
+    phone_number = models.CharField(max_length=100)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
