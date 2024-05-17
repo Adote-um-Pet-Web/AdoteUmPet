115413c05cdc50cfb1f835da09d7c3c6e09467b2
wendellast <wendelalves01a@hotmail.com>
2024-05-17 15:51:03 -0300

    register: email-user-ok :art:

--- userauth/forms.py
+++ userauth/forms.py
@@ -5,7 +5,7 @@ from .models import User
 
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Username"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Nome Completo"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder":"Email"}))
     phone_number = forms.IntegerField(widget=forms.NumberInput(attrs={"placeholder":"Número de Telefone"}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}))
