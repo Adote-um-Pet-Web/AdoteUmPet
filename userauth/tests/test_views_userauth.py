bf371067320704085337041a3818a0f2a6eaf2d1
wendellast <wendelalves01a@hotmail.com>
2024-05-21 07:24:40 -0700

    add tests

--- userauth/tests/test_views_userauth.py
+++ userauth/tests/test_views_userauth.py
@@ -1,8 +1,10 @@
-from django.test import TestCase
-from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.contrib.messages import get_messages
+from django.test import TestCase
+from django.urls import reverse
+
 from userauth.models import User
+
 
 class UserAuthViewsTest(TestCase):
     def setUp(self):
@@ -19,7 +21,9 @@ class UserAuthViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
         response = self.client.post(reverse("userauths:sign-up"), self.user_data)
-        self.assertEqual(response.status_code, 200)  # Redirects after successful registration
+        self.assertEqual(
+            response.status_code, 200
+        )  # Redirects after successful registration
         self.assertTrue(User.objects.filter(email=self.user_data["email"]).exists())
 
     def test_login_view(self):
@@ -27,20 +31,28 @@ class UserAuthViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
         # Test logging in with correct credentials
-        response = self.client.post(reverse("userauths:sign-in"), {"email": "test@example.com", "password": "testpassword"})
+        response = self.client.post(
+            reverse("userauths:sign-in"),
+            {"email": "test@example.com", "password": "testpassword"},
+        )
         self.assertEqual(response.status_code, 302)  # Redirects after successful login
 
         # Check if user is authenticated
-        self.assertTrue(self.client.session['_auth_user_id'])  # User should be authenticated
+        self.assertTrue(
+            self.client.session["_auth_user_id"]
+        )  # User should be authenticated
 
         # Redirects to the expected URL
-        self.assertRedirects(response, reverse('pets:index'))
+        self.assertRedirects(response, reverse("pets:index"))
 
         # Test logging in with incorrect credentials
-        response = self.client.post(reverse("userauths:sign-in"), {"email": "test@example.com", "password": "wrongpassword"})
-        self.assertEqual(response.status_code, 302)  # Redirects after unsuccessful login
-
-
+        response = self.client.post(
+            reverse("userauths:sign-in"),
+            {"email": "test@example.com", "password": "wrongpassword"},
+        )
+        self.assertEqual(
+            response.status_code, 302
+        )  # Redirects after unsuccessful login
 
     def test_logout_view(self):
         # Log in the user first
@@ -62,4 +74,3 @@ class UserAuthViewsTest(TestCase):
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), "You have been logged out.")
-
