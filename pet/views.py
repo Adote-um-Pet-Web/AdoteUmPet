9fb8ce34b002172649e5e4f56dc7ff565e76a095
wendellast <wendelalves01a@hotmail.com>
2024-05-24 14:58:26 -0300

    page pet-created: :hammer:

--- pet/views.py
+++ pet/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 from . import models
 
 
-class PageView(ListView):
+class PagePetIndex(ListView):
     model = models.Pet
     template_name = "index.html"
     context_object_name = "pet"
+
+
+class PageNewPet(LoginRequiredMixin, CreateView):
+    model = models.Pet
+    fields = ['name', 'species', 'breed', 'age', 'color',  'sex', 'size', 'weight','history','observations','image_profile','adopted']
+    success_url = reverse_lazy('pets:index')
+    template_name = 'registerPet.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PageNewPet, self).form_valid(form)
