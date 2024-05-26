db94c9719cfaff89dc8268c8570757de92bdba70
wendellast <wendelalves01a@hotmail.com>
2024-05-26 18:17:50 -0300

    feat: create a pet

--- userauth/models.py
+++ userauth/models.py
@@ -36,34 +36,34 @@ def user_directory_path(instace, filename):
     return "user_{0}/{1}".format(instace.user.id, filename)
 
 
-class Profile(models.Model):
-    id = models.UUIDField(
-        primary_key=True, unique=True, default=uuid.uuid4, editable=False
-    )
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="account")
-    account_status = models.CharField(
-        max_length=100, choices=ACCOUNT_STATUS, default="in-active"
-    )
-    red_code = ShortUUIDField(
-        unique=True, length=10, max_length=20, alphabet="abcdefgh1234567890"
-    )
-    date = models.DateTimeField(auto_now_add=True)
+# class Profile(models.Model):
+#     id = models.UUIDField(
+#         primary_key=True, unique=True, default=uuid.uuid4, editable=False
+#     )
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="account")
+#     account_status = models.CharField(
+#         max_length=100, choices=ACCOUNT_STATUS, default="in-active"
+#     )
+#     red_code = ShortUUIDField(
+#         unique=True, length=10, max_length=20, alphabet="abcdefgh1234567890"
+#     )
+#     date = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ["-date"]
+#     class Meta:
+#         ordering = ["-date"]
 
-    def __str__(self):
-        return f"{self.user}"
-
-
-def create_account(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+#     def __str__(self):
+#         return f"{self.user}"
 
 
-def save_account(sender, instance, **kwargs):
-    instance.account.save()
+# def create_account(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
 
 
-post_save.connect(create_account, sender=User)
-post_save.connect(save_account, sender=User)
+# def save_account(sender, instance, **kwargs):
+#     instance.account.save()
+
+
+# post_save.connect(create_account, sender=User)
+# post_save.connect(save_account, sender=User)
