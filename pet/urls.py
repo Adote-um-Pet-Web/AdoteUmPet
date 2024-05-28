a8c6d6b9760cd7e264dde919a40d6a15530d1ea6
wendellast <wendelalves01a@hotmail.com>
2024-05-27 21:00:51 -0300

    bugs: fix

--- pet/urls.py
+++ pet/urls.py
@@ -17,12 +17,13 @@ register_converter(UUIDConverter, 'uuid')
 
 urlpatterns = [
     path("", views.PagePetIndex.as_view(), name="index"),
-    path("newpet/", views.CreatePetView.as_view(), name="newpet"),
-    path('pet/<int:pk>/', views.PageDetailPet.as_view(), name='pet-detail'),
+    path("navform/", views.PageNavForm.as_view(), name="navform"),
 
+    path("create_pet/", views.CreatePetView.as_view(), name="create_pet"),
     path('create_history_pet/<uuid:pet_id>/', views.CreateHistoryPetView.as_view(), name='create_history_pet'),
     path('create_medical_record/<uuid:pet_id>/', views.CreateMedicalRecordView.as_view(), name='create_medical_record'),
     path('create_images_pets/<uuid:pet_id>/', views.CreateImagesPetsView.as_view(), name='create_images_pets'),
+    path('pet/<int:pk>/', views.PageDetailPet.as_view(), name='pet-detail'),
 
 ]
 
--- pet/views.py
+++ pet/views.py
@@ -2,87 +2,72 @@
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, DetailView
-from django.urls import reverse, reverse_lazy
+from django.urls import  reverse_lazy
 from . import models
-
 from django.views.generic import CreateView
-from .forms import PetForm, HistoryPetForm, MedicalRecordForm
+from .forms import PetForm, HistoryPetForm, MedicalRecordForm, ImagesPetsForm
 from .models import Pet
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect
 
-from django.views.generic.edit import FormView
 
-from .forms import ImagesPetsForm
-
-from .models import ImagesPets, Pet
-
-from uuid import UUID
 class PagePetIndex(ListView):
     model = models.Pet
     template_name = "index.html"
     context_object_name = "pet"
 
 
-class PageNewPet(LoginRequiredMixin, CreateView):
+class PageNavForm(LoginRequiredMixin, ListView):
     model = models.Pet
-    fields = ['name', 'species', 'breed', 'age', 'color',  'sex', 'size', 'weight','adopted']
-    success_url = reverse_lazy('pets:index')
-    template_name = 'registerPet.html'
-
-
-
-    def form_valid(self, form):
-        form.instance.owner = self.request.user
-        return super().form_valid(form)
+    template_name = 'navForm.html'
+    context_object_name = "pet"
 
 
 
 class CreatePetView(CreateView):
     form_class = PetForm
-    template_name = 'registerPet.html'
-    success_url = reverse_lazy('pets:index')
+    template_name = 'createPet.html'
     context_object_name = "pet"
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
-        return super().form_valid(form)
+
+        self.object = form.save()
+
+        return redirect('pets:create_history_pet', pet_id=self.object.id)
 
 class CreateHistoryPetView(CreateView):
     form_class = HistoryPetForm
     template_name = 'createHistory.html'
 
     def get_success_url(self):
-        return reverse_lazy('pets:index')
+        return reverse_lazy('pets:create_medical_record', kwargs={'pet_id': self.kwargs['pet_id']})
 
     def form_valid(self, form):
         form.instance.id_pet = Pet.objects.get(id=self.kwargs['pet_id'])
         return super().form_valid(form)
+
 
 class CreateMedicalRecordView(CreateView):
     form_class = MedicalRecordForm
     template_name = 'createMedicalRecord.html'
 
     def get_success_url(self):
-        return reverse_lazy('pets:index')
+        return reverse_lazy('pets:create_images_pets', kwargs={'pet_id': self.kwargs['pet_id']})
 
     def form_valid(self, form):
         form.instance.id_pet = Pet.objects.get(id=self.kwargs['pet_id'])
         return super().form_valid(form)
 
 class CreateImagesPetsView(CreateView):
-
     form_class = ImagesPetsForm
-
     template_name = 'createImagePets.html'
 
 
     def get_success_url(self):
-
-        return reverse_lazy('pets:index')  # redirect to a list view or any other page
+        return reverse_lazy('pets:index')
 
 
     def form_valid(self, form):
-
         form.instance.id_pet = Pet.objects.get(id=self.kwargs['pet_id'])
         return super().form_valid(form)
 
