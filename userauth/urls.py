0610a39e685190b61f3b2a598e8c4e8857203854
wendellast <wendelalves01a@hotmail.com>
2024-05-31 11:56:29 -0300

    user : id>uuid

--- userauth/urls.py
+++ userauth/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("sign-up/", views.SignUpView.as_view(), name="sign-up"),
     path("sign-in/", views.login_view, name="sign-in"),
     path("sign-out/", views.logout_view, name="sign-out"),
-    path('user-update/<int:pk>/', views.PageUpdateUser.as_view(), name='user-update'),
+    path('user-update/<uuid:pk>/', views.PageUpdateUser.as_view(), name='user-update'),
 ]
