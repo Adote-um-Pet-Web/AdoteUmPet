0f13d5b2993e8b8f27ca4b372f17a6c0e509a6b5
wendellast <wendelalves01a@hotmail.com>
2024-06-05 21:24:11 -0300

    migrations

---
+++ adoption/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name="adoption"
+
+urlpatterns = [
+    path('contact/<uuid:pet_id>/', views.AdoptionContactPage.as_view(), name='adoption-contact'),
+    path("adoption-pet/<uuid:pk>/", views.AdoptPetView.as_view(),  name="adoption-pet"),
+    path("adoption-list/", views.AdoptionPetsList.as_view(), name="adoption-list"),
+
+]
