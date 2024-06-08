23a8f75b8113788359e1f0160a6d5b98bbfba82a
wendellast <wendelalves01a@hotmail.com>
2024-06-08 20:23:01 -0300

    new-check-contant

--- adoption/urls.py
+++ adoption/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     ),
     path("adoption-pet/<uuid:pk>/", views.AdoptPetView.as_view(), name="adoption-pet"),
     path("adoption-list/", views.AdoptionPetsList.as_view(), name="adoption-list"),
+    path('check-fields/', views.CheckFieldsView.as_view(), name='check-fields'),
 ]
