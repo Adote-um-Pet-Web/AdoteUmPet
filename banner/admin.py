afd3586ba677b723149049bdca05031c3efa1d2b
wendellast <wendelalves01a@hotmail.com>
2024-06-10 14:16:03 -0300

    add new banner app

---
+++ banner/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models
+
+
+class BannerImagesLine(admin.StackedInline):
+    model = models.BannerImagens
+    extra = 1
+
+admin.site.register(models.BannerImagens)
