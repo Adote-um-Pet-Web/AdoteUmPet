614a1c651dff23af3ab0aa6e5849aeee2ea822a9
wendellast <wendelalves01a@hotmail.com>
2024-06-11 22:34:55 -0300

    collect: static

--- static/assets/js/banner.js
+++ static/assets/js/banner.js
@@ -1,58 +1,58 @@
 
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-    const slidersContainer = document.getElementById("sliders");
-    const slides = document.querySelectorAll(".slide");
-    const sliderStatus = document.getElementById("sliderStatus");
-
-
-    let currentSlide = 0;
-    let slideInterval = setInterval(nextSlide, 3000);
+//     const slidersContainer = document.getElementById("sliders");
+//     const slides = document.querySelectorAll(".slide");
+//     const sliderStatus = document.getElementById("sliderStatus");
 
 
-    function updateSliderStatus() {
-        sliderStatus.innerHTML = "";
-        slides.forEach((_, index) => {
-            const dot = document.createElement("span");
-            dot.classList.add("dot");
-            if (index === currentSlide) {
-                dot.classList.add("active");
-            }
-            dot.addEventListener("click", () => {
-                goToSlide(index);
-            });
-            sliderStatus.appendChild(dot);
-        });
-    }
+//     let currentSlide = 0;
+//     let slideInterval = setInterval(nextSlide, 3000);
 
 
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % slides.length;
-        goToSlide(currentSlide);
-    }
+//     function updateSliderStatus() {
+//         sliderStatus.innerHTML = "";
+//         slides.forEach((_, index) => {
+//             const dot = document.createElement("span");
+//             dot.classList.add("dot");
+//             if (index === currentSlide) {
+//                 dot.classList.add("active");
+//             }
+//             dot.addEventListener("click", () => {
+//                 goToSlide(index);
+//             });
+//             sliderStatus.appendChild(dot);
+//         });
+//     }
 
 
-    function goToSlide(index) {
-        currentSlide = index;
-
-        slides.forEach((slide) => {
-            slide.classList.remove("active");
-        });
-
-        slides[currentSlide].classList.add("active");
-        updateSliderStatus();
-    }
-
-    goToSlide(0);
+//     function nextSlide() {
+//         currentSlide = (currentSlide + 1) % slides.length;
+//         goToSlide(currentSlide);
+//     }
 
 
-    bannerArea.addEventListener("mouseenter", () => {
-        clearInterval(slideInterval);
-    });
+//     function goToSlide(index) {
+//         currentSlide = index;
 
-    bannerArea.addEventListener("mouseleave", () => {
-        slideInterval = setInterval(nextSlide, 3000);
-    });
+//         slides.forEach((slide) => {
+//             slide.classList.remove("active");
+//         });
 
-    updateSliderStatus();
-});
+//         slides[currentSlide].classList.add("active");
+//         updateSliderStatus();
+//     }
+
+//     goToSlide(0);
+
+
+//     bannerArea.addEventListener("mouseenter", () => {
+//         clearInterval(slideInterval);
+//     });
+
+//     bannerArea.addEventListener("mouseleave", () => {
+//         slideInterval = setInterval(nextSlide, 3000);
+//     });
+
+//     updateSliderStatus();
+// });
