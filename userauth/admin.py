05ab9b6280ba8e88ea5ae9ee9e0c9457a9be2bd0
wendellast <wendelalves01a@hotmail.com>
2024-06-22 13:30:42 -0300

    dash-board-admin: todo

--- userauth/admin.py
+++ userauth/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
-from . import models
-
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
-admin.site.register(models.User)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('username', 'phone_number', 'instagram_field', 'facebook_field', 'image_user_profile')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'username', 'is_staff')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+
