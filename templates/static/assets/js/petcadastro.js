db4117e7ad974bd5563967a3acff34f27d368113
Andra <camileguimaraes463@gmail.com>
2024-06-25 16:40:01 -0300

    tamanho dos cards do index para telas maiores arrumado

--- templates/static/assets/js/petcadastro.js
+++ templates/static/assets/js/petcadastro.js
@@ -1,57 +1,83 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const profilePetInput = document.getElementById("profilePet");
-//     const mainPhotoPreview = document.getElementById("main-photo-preview");
+const profilePetInput = document.getElementById("profilePet");
+const mainPhotoPreview = document.getElementById("main-photo-preview");
+const mainPhotoPreviewImg = document.getElementById("main-photo-preview-img");
 
-//     profilePetInput.addEventListener("change", function () {
-//         const file = profilePetInput.files[0];
-//         const ibtn = document.getElementById("ibtn");
-//         if (file) {
-//             const reader = new FileReader();
-//             reader.onload = function (e) {
-//                 mainPhotoPreview.style.backgroundImage = `url(${e.target.result})`;
-//                 mainPhotoPreview.style.backgroundSize = "cover";
-//                 mainPhotoPreview.style.backgroundPosition = "center";
-//                 ibtn.style.display = "none";
-//             };
-//             reader.readAsDataURL(file);
-//         }
-//     });
+profilePetInput.addEventListener("change", function () {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function () {
+            mainPhotoPreviewImg.src = reader.result;
+            mainPhotoPreviewImg.style.display = "block";
+            ibtn.style.display = "none";
+        };
+        reader.readAsDataURL(file);
+    } else {
+        mainPhotoPreviewImg.src = "#";
+        mainPhotoPreviewImg.style.display = "none";
+        ibtn.style.display = "block";
+    }
+});
 
-//     const extraPhotoInputs = [
-//         document.getElementById("image_pet_datail1"),
-//         document.getElementById("image_pet_datail2"),
-//         document.getElementById("image_pet_datail3")
-//     ];
+const imagePetDatail1Input = document.getElementById("image_pet_datail1");
+const extraPhotoPreview1 = document.getElementById("extra-photo-preview-1");
+const extraPhotoPreviewImg1 = document.getElementById("extra-photo-preview-img-1");
 
-//     const extraPhotoPreviews = [
-//         document.getElementById("extra-photo-preview-1"),
-//         document.getElementById("extra-photo-preview-2"),
-//         document.getElementById("extra-photo-preview-3")
-//     ];
+imagePetDatail1Input.addEventListener("change", function () {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function () {
+            extraPhotoPreviewImg1.src = reader.result;
+            extraPhotoPreviewImg1.style.display = "block";
+            ibtn1.style.display = "none";
+        };
+        reader.readAsDataURL(file);
+    } else {
+        extraPhotoPreviewImg1.src = "#";
+        extraPhotoPreviewImg1.style.display = "none";
+        ibtn1.style.display = "block";
+    }
+});
 
-//     const ibtns = [
-//         document.getElementById("ibtn1"),
-//         document.getElementById("ibtn2"),
-//         document.getElementById("ibtn3")
-//     ];
+const imagePetDatail2Input = document.getElementById("image_pet_datail2");
+const extraPhotoPreview2 = document.getElementById("extra-photo-preview-2");
+const extraPhotoPreviewImg2 = document.getElementById("extra-photo-preview-img-2");
 
-//     extraPhotoInputs.forEach((input, index) => {
-//         input.addEventListener("change", function () {
-//             const file = input.files[0];
-//             const preview = extraPhotoPreviews[index];
-//             const ibtn = ibtns[index];
-//             if (file) {
-//                 const reader = new FileReader();
-//                 reader.onload = function (e) {
-//                     preview.style.backgroundImage = `url(${e.target.result})`;
-//                     preview.style.backgroundSize = "cover";
-//                     preview.style.backgroundPosition = "center";
-//                     ibtn.style.display = "none";
-//                 };
-//                 reader.readAsDataURL(file);
-//             }
-//         });
-//     });
-// });
+imagePetDatail2Input.addEventListener("change", function () {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function () {
+            extraPhotoPreviewImg2.src = reader.result;
+            extraPhotoPreviewImg2.style.display = "block";
+            ibtn2.style.display = "none";
+        };
+        reader.readAsDataURL(file);
+    } else {
+        extraPhotoPreviewImg2.src = "#";
+        extraPhotoPreviewImg2.style.display = "none";
+        ibtn2.style.display = "block";
+    }
+});
 
+const imagePetDatail3Input = document.getElementById("image_pet_datail3");
+const extraPhotoPreview3 = document.getElementById("extra-photo-preview-3");
+const extraPhotoPreviewImg3 = document.getElementById("extra-photo-preview-img-3");
 
+imagePetDatail3Input.addEventListener("change", function () {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function () {
+            extraPhotoPreviewImg3.src = reader.result;
+            extraPhotoPreviewImg3.style.display = "block";
+            ibtn3.style.display = "none";
+        };
+        reader.readAsDataURL(file);
+    } else {
+        extraPhotoPreviewImg3.src = "#";
+        extraPhotoPreviewImg3.style.display = "none";
+        ibtn3.style.display = "block";
+    }
+});
