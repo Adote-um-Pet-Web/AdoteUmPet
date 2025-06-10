7473f7602b495ead94dad98e1777f59fc2065d3f
Andra-sun <camileguimaraes463@gmail.com>
2025-06-10 15:16:37 -0300

    refactor(theme): melhora seleção de cor e tema

--- templates/static/assets/js/theme.js
+++ templates/static/assets/js/theme.js
@@ -1,80 +1,80 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const colorButtons = document.querySelectorAll('section.configSection ul:nth-of-type(1) li');
-    const themeButtons = document.querySelectorAll('section.configSection ul:nth-of-type(2) li');
-    const applyColorButton = document.querySelectorAll('section.configSection button')[0];
-    const applyThemeButton = document.querySelectorAll('section.configSection button')[1];
+document.addEventListener("DOMContentLoaded", () => {
+    const colorButtons = document.querySelectorAll(".color-option");
+    const themeButtons = document.querySelectorAll(".theme-option");
+    const applyColorButton = document.querySelectorAll(
+        "section.configSection button"
+    )[0];
+    const applyThemeButton = document.querySelectorAll(
+        "section.configSection button"
+    )[1];
 
-    const defaultColor = 'padrao';
-    const defaultTheme = 'auto';
+    const defaultColor = "padrao";
+    const defaultTheme = "auto";
 
-    let selectedColor = localStorage.getItem('selectedColor') || defaultColor;
-    let selectedTheme = localStorage.getItem('selectedTheme') || defaultTheme;
+    let selectedColor = localStorage.getItem("selectedColor") || defaultColor;
+    let selectedTheme = localStorage.getItem("selectedTheme") || defaultTheme;
 
-    document.body.setAttribute('data-color', selectedColor);
-    if (selectedTheme === 'auto') {
-        const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-        document.body.setAttribute('data-theme', prefersDarkScheme ? 'dark' : 'light');
+    document.body.setAttribute("data-color", selectedColor);
+    if (selectedTheme === "auto") {
+        const prefersDarkScheme = window.matchMedia(
+            "(prefers-color-scheme: dark)"
+        ).matches;
+        document.body.setAttribute(
+            "data-theme",
+            prefersDarkScheme ? "dark" : "light"
+        );
     } else {
-        document.body.setAttribute('data-theme', selectedTheme);
+        document.body.setAttribute("data-theme", selectedTheme);
     }
 
-    colorButtons.forEach(button => {
+    colorButtons.forEach((button) => {
         if (button.textContent.toLowerCase() === getColorName(selectedColor)) {
-            button.classList.add('selected');
+            button.classList.add("selected");
         }
     });
 
-    themeButtons.forEach(button => {
+    themeButtons.forEach((button) => {
         if (button.textContent.toLowerCase() === getThemeName(selectedTheme)) {
-            button.classList.add('selected');
+            button.classList.add("selected");
         }
     });
 
-    colorButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            colorButtons.forEach(btn => btn.classList.remove('selected'));
-            button.classList.add('selected');
-            const colorText = button.textContent.toLowerCase();
-            if (colorText === 'amarelo') {
-                selectedColor = 'padrao';
-            } else if (colorText === 'azul') {
-                selectedColor = 'invertido';
-            } else if (colorText === 'laranja') {
-                selectedColor = 'laranja';
-            }
-            localStorage.setItem('selectedColor', selectedColor);
+    colorButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            colorButtons.forEach((btn) => btn.classList.remove("selected"));
+            button.classList.add("selected");
+            selectedColor = button.getAttribute("data-color");
+            localStorage.setItem("selectedColor", selectedColor);
         });
     });
 
-    themeButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            themeButtons.forEach(btn => btn.classList.remove('selected'));
-            button.classList.add('selected');
-            const themeText = button.textContent.toLowerCase();
-            if (themeText === 'claro') {
-                selectedTheme = 'light';
-            } else if (themeText === 'escuro') {
-                selectedTheme = 'dark';
-            } else if (themeText === 'automático') {
-                selectedTheme = 'auto';
-            }
-            localStorage.setItem('selectedTheme', selectedTheme);
+    themeButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            themeButtons.forEach((btn) => btn.classList.remove("selected"));
+            button.classList.add("selected");
+            selectedTheme = button.getAttribute("data-theme");
+            localStorage.setItem("selectedTheme", selectedTheme);
         });
     });
 
-    applyColorButton.addEventListener('click', () => {
+    applyColorButton.addEventListener("click", () => {
         if (selectedColor) {
-            document.body.setAttribute('data-color', selectedColor);
+            document.body.setAttribute("data-color", selectedColor);
         }
     });
 
-    applyThemeButton.addEventListener('click', () => {
+    applyThemeButton.addEventListener("click", () => {
         if (selectedTheme) {
-            if (selectedTheme === 'auto') {
-                const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-                document.body.setAttribute('data-theme', prefersDarkScheme ? 'dark' : 'light');
+            if (selectedTheme === "auto") {
+                const prefersDarkScheme = window.matchMedia(
+                    "(prefers-color-scheme: dark)"
+                ).matches;
+                document.body.setAttribute(
+                    "data-theme",
+                    prefersDarkScheme ? "dark" : "light"
+                );
             } else {
-                document.body.setAttribute('data-theme', selectedTheme);
+                document.body.setAttribute("data-theme", selectedTheme);
             }
         }
     });
@@ -85,13 +85,13 @@ function capitalizeFirstLetter(string) {
 }
 
 function getColorName(color) {
-    if (color === 'padrao') return 'amarelo';
-    if (color === 'invertido') return 'azul';
+    if (color === "padrao") return "amarelo";
+    if (color === "invertido") return "azul";
     return color;
 }
 
 function getThemeName(theme) {
-    if (theme === 'light') return 'claro';
-    if (theme === 'dark') return 'escuro';
-    return 'automático';
+    if (theme === "light") return "claro";
+    if (theme === "dark") return "escuro";
+    return "automático";
 }
